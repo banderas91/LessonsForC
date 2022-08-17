1a3e96755ec956e75326babe4973fe8cbdeb9674
K_A_A <banderas91@mail.ru>
2022-08-17 09:51:57 +0300

    условие if else

---
+++ lesson005_if_else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша") // переводит все буквы в консоли в нижний регистр
+{
+    Console.WriteLine("Ура! Это же Маша!");
+}
+else
+{
+    Console.Write("Привет ");
+    Console.WriteLine(username);
+}
