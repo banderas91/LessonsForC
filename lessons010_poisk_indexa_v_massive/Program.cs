ff5ba8b6658ed1026a3007fdcf372c9ba7d2ec93
K_A_A <banderas91@mail.ru>
2022-08-22 23:37:00 +0300

    поиск_индекса_в_массиве

---
+++ lessons010_poisk_indexa_v_massive/Program.cs
@@ -0,0 +1,18 @@
+﻿
+int[] array = {1, 42, 23, 41, 8, 16, 27, 18};
+
+int n = array.Length; // возвращает количество элементов в массиве
+int find = 18;
+int index = 0;
+
+while (index < n)
+{
+
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+
+}
