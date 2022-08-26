76e7f1d632bc6258718659464d45cdd8e9164725
K_A_A <banderas91@mail.ru>
2022-08-26 11:55:43 +0300

    семинар_3

---
+++ Seminar003_srednee_aref_elementov_massiva/Program.cs
@@ -0,0 +1,22 @@
+﻿//1 создаем массив
+int N = Convert.ToInt32(Console.ReadLine());
+int [] array = new int [N];
+
+// 2. заполняем случайныими числами
+
+int index = 0;
+while(index <N)
+{
+    array[index] = new Random().Next(1, 11); 
+    Console.Write(array[index]+ "   ");
+    index++;
+}
+
+// 3. Среднее арефметическое элементов массива
+int sum = 0;
+for(int i=0; i < N; i++)
+{
+    sum += array[i];
+}
+double mean = (double)sum / N;
+Console.WriteLine(mean);
---
+++ seminar003_zapolnenie_massiva_kvadratami_chisel/Program.cs
@@ -0,0 +1,16 @@
+﻿//Напишите программу, которая принимает на вход натуральное число N и заполняет массив квадратами чисел от 1 до N. полученный массив вывести на экран.
+
+Console.Write("введите число");
+int N =Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int [N];
+
+int index = 0;
+int num = 1;
+while(index < N)
+{
+    array[index] = num * num;
+    Console.Write(array[index] + " ");
+    num++;
+    index++;
+}
